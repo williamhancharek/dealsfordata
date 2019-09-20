1281c9e2ca3a7cb63e587403f6583129d1a5ac4c
William Hancharek <william.hancharek@gmail.com>
2019-09-20 16:59:36 -0400

    put in bootstrap and stimulus

--- Gemfile
+++ Gemfile
@@ -15,6 +15,12 @@ gem 'sass-rails', '~> 5'
 gem 'webpacker', '~> 4.0'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
+
+gem 'devise'
+gem 'cancancan'
+gem 'simple_form'
+gem 'geo_coder'
+
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,33 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//loading StimulusJS
+import { Application } from 'stimulus'
+import { definitionsFromContext } from 'stimulus/webpack-helpers'
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
+
+//loading css files
+import '../stylesheets/application'
+
+//template
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+//loading bootstrap
+import "bootstrap"
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+
+//loading images
+const images = require.context('../images', true)
