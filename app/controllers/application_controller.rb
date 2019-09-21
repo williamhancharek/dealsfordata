b31c26802ce5a28e0c0c7213abdb81d13d6acbbd
William Hancharek <william.hancharek@gmail.com>
2019-09-21 00:08:48 -0400

    enforce sign in requirements to see inside"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: :HomeController::index
+
   def after_sign_in_path_for(resource)
     if resource.role == 'customer'
       customer_path(current_user)
