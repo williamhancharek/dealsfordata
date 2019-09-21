f7e2c76b3389be7f53673e0ccde5b012c8118ee3
William Hancharek <william.hancharek@gmail.com>
2019-09-21 00:57:30 -0400

    set up requirement to log in, and set up that offers belongs to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: :HomeController::index
+  before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
     if resource.role == 'customer'
