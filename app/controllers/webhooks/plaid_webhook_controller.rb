a36275e6ad8cbb9f15eac4c32bbcfc03ec59ba4a
William Hancharek <william.hancharek@gmail.com>
2019-11-08 18:13:57 -0500

    removing plaid.rb for now to see if I can upload to heroku

--- app/controllers/webhooks/plaid_webhook_controller.rb
+++ app/controllers/webhooks/plaid_webhook_controller.rb
@@ -1,7 +1,6 @@
 class Webhooks::PlaidWebhookController < ApplicationController
 
   def index
-    binding.pry_remote
     #I don't know how to do this until I can receive a webhook from plaid
 
     #TODO this is supposed to recieve webhooks from plaid, and then process them
--- config/initializers/plaid.rb
+++ config/initializers/plaid.rb
@@ -1,7 +1,7 @@
-require 'plaid'
-::Client.new(env: ENV['PLAID_ENV'],
-                           client_id: ENV['PLAID_CLIENT_ID'],
-                           secret: ENV['PLAID_SECRET'],
-                           public_key: ENV['PLAID_PUBLIC_KEY'])
-
-#TODO is this bad?  to make ::Client a global constant...?
+# require 'plaid'
+# client = Plaid::Client.new(env: ENV['PLAID_ENV'],
+#                            client_id: ENV['PLAID_CLIENT_ID'],
+#                            secret: ENV['PLAID_SECRET'],
+#                            public_key: ENV['PLAID_PUBLIC_KEY'])
+#
+# #TODO is this bad?  to make ::Client a global constant...?
