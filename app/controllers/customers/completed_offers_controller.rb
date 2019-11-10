e36994bf1cf8ae6979c8b08b6032f77eaf6040c5
William Hancharek <william.hancharek@gmail.com>
2019-11-10 13:00:04 -0500

    moved completed_controller to completed_offers_controller

--- app/controllers/customers/completed_offers_controller.rb
+++ app/controllers/customers/completed_offers_controller.rb
@@ -1,4 +1,4 @@
-class Customers::Offers::CompletedController < ApplicationController
+class Customers::CompletedOffersController < ApplicationController
   before_action only: [:index] do
     set_user("customer", params[:customer_id])
   end
---
+++ app/helpers/customers/completed_offers_helper.rb
@@ -0,0 +1,2 @@
+module Customers::CompletedOffersHelper
+end
