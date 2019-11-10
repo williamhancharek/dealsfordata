ff63bdf7d6b0bd94832694990998c6dd6a7fdfdc
William Hancharek <william.hancharek@gmail.com>
2019-11-10 16:08:24 -0500

    set up create a new box page for customer

--- app/controllers/customer/boxes_controller.rb
+++ app/controllers/customer/boxes_controller.rb
@@ -1,17 +1,37 @@
 class Customer::BoxesController < ApplicationController
   before_action only: [:index] do
-    {set_instance(instance:'customer', id:params[:user_id],object:User)}
+    set_instance(instance:'customer',
+                        id:params[:user_id],
+                    object: :User)
+  end
+
+  before_action only: [:show, :edit, :update, :destroy] do
+    set_instance(instance:'box',id:params[:id],object: :Box)
   end
 
   def index
-
+    @boxes = @customer.boxes
   end
+
+  def new
+    @box = Box.new
+  end
+
+  def create
+    
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
 
   private
 
   def boxes_params
     #todo
   end
-
 
 end
