f7bc1713e9e6539f9adebc71e8af098f57708bf5
William Hancharek <william.hancharek@gmail.com>
2019-11-26 00:30:12 -0500

    cleaned up box helper method

--- app/helpers/customer/boxes_helper.rb
+++ app/helpers/customer/boxes_helper.rb
@@ -2,10 +2,11 @@ module Customer::BoxesHelper
 
   def addBoxLinks(box:, subscriber:)
     #if subscribed is true or false, then we're dealing with foreign boxes
-    if subscriber.subscribed?(box)   == nil
+    if subscriber == nil
       render partial: "customer_box_links", locals: {box: box}
     else
-      render partial: "foreign_box_links", locals: {subscriber: subscriber, box: box}
+      subscribed = subscriber.subscribed?(box)
+      render partial: "foreign_box_links", locals: {subscriber: subscriber, box: box, subscribed: subscribed}
     end
   end
 
