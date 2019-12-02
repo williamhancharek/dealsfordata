4e5e1d8dc5ab5554c873859b02d00f5868ee3f72
William Hancharek <william.hancharek@gmail.com>
2019-12-02 12:07:33 -0500

    fixing heroku email sending

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'dealsfordata.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+# ActionMailer::Base.smtp_settings = {
+#   :user_name => ENV['SENDGRID_USERNAME'],
+#   :password => ENV['SENDGRID_PASSWORD'],
+#   :domain => 'dealsfordata.com',
+#   :address => 'smtp.sendgrid.net',
+#   :port => 587,
+#   :authentication => :plain,
+#   :enable_starttls_auto => true
+# }
+ #TODO I disabled this because I don't think I need to initialize this right away
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,11 +60,25 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "dealsfordata_production"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = false #TODO change this later for production
+
+  config.action_mailer.default_url_options = { :host =&gt; 'serene-everglades-06595.herokuapp.com'}
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.sendgrid.net",
+  port: 25,
+  domain: "heroku.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["SENDGRID_USERNAME"],
+  password: ENV["SENDGRID_PASSWORD"]
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true #TODO change this back for real launch
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
