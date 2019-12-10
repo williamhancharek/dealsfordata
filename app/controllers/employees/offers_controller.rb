0f4c2cb30800f9bd8fa99213f2881aa1ca7bc461
William Hancharek <william.hancharek@gmail.com>
2019-12-10 12:23:45 -0500

    added constraints to accessing sidekiq web ui to just admins

--- app/controllers/employees/offers_controller.rb
+++ app/controllers/employees/offers_controller.rb
@@ -25,7 +25,7 @@ class Employees::OffersController < ApplicationController
     respond_to do |format|
       if @offer.save
         if @offer.box.allow_email = true
-          OfferMailer.with(offer_id: @offer.id).offer_email.deliver_later
+          OfferMailer.with(offer_id: @offer.id).offer_email.deliver_now #TODO change once I'm stable
         end
         format.html {redirect_back fallback_location: :new, notice: "offer was successfully created"}
         format.json {render :new, status: :create, location: @user}
