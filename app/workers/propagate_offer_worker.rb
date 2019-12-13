1135ef5fbd3590040204438709ea1d07aea75c8f
William Hancharek <william.hancharek@gmail.com>
2019-12-13 17:37:40 -0500

    added .transaction so that there's rollback for the workers

--- app/workers/propagate_offer_worker.rb
+++ app/workers/propagate_offer_worker.rb
@@ -10,19 +10,21 @@ class PropagateOfferWorker
     box.subscribers.each do |sub|
       new_offer = sub.offers.find_or_create_by(original_offer_id: offer_id)
       if (new_offer.referrer.nil? || !(new_offer.referrer.include?(box.id))) #THIS ENSURE WE DON'T HAVE CIRCULAR REFERRALS
-        new_offer.referrer ||= []
-        new_offer.referrer.push(box.id)
-        new_offer.description ||= offer.description
-        new_offer.title ||= offer.title
-        new_offer.tags ||= offer.tags
-        new_offer.options ||=offer.options
-        new_offer.public_options ||= offer.public_options
-        new_offer.box_id ||=sub.id
-        new_offer.campaign_id ||= offer.campaign.id
-        new_offer.received_commentary ||= offer.commentary
-        new_offer.original_offer_id ||= offer.id
-        new_offer.save
-        OfferMailer.with(offer_id: new_offer.id).referral_email.deliver_later
+        ActiveRecord::Base.transaction do
+          new_offer.referrer ||= []
+          new_offer.referrer.push(box.id)
+          new_offer.description ||= offer.description
+          new_offer.title ||= offer.title
+          new_offer.tags ||= offer.tags
+          new_offer.options ||=offer.options
+          new_offer.public_options ||= offer.public_options
+          new_offer.box_id ||=sub.id
+          new_offer.campaign_id ||= offer.campaign.id
+          new_offer.received_commentary ||= offer.commentary
+          new_offer.original_offer_id ||= offer.id
+          new_offer.save!
+          OfferMailer.with(offer_id: new_offer.id).referral_email.deliver_later
+        end
       end
     end
   end
