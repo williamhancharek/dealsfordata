a2e6a0ecf1e511f757f1fc26c330396f0c40b022
William Hancharek <william.hancharek@gmail.com>
2020-01-04 13:52:11 -0500

    corrected approveofferworker

--- app/workers/approve_offer_worker.rb
+++ app/workers/approve_offer_worker.rb
@@ -4,9 +4,9 @@ class ApproveOfferWorker
   def perform(offer_id)
     offer = Offer.find(offer_id)
 
-    if offer.approved == true && offer.status == true && offer.box.allow_email == true && (offer.email_sent == nil || offer.email_sent == false)
+    if offer.approved == true && offer.active == true && offer.box.allow_email == true && (offer.email_sent == nil || offer.email_sent == false)
       ActiveRecord::Base.transaction do
-        OfferMailer.with(offer_id: @offer.id).offer_email.deliver_later
+        OfferMailer.with(offer_id: offer.id).offer_email.deliver_later
         offer.email_sent = true
         offer.save
       end
