369ae8e53ff7ab096a4967f41c871c6a59ff941f
William Hancharek <william.hancharek@gmail.com>
2020-01-11 23:23:20 -0500

    added new logos by Jess!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,24 +8,24 @@ module ApplicationHelper
   def tabsorter(active:, box: )
     if user_signed_in?
       if box.present?
-        render partial: "partials/tabs", locals: {active: active, box: box}
+        render partial: "partials/tabs_box", locals: {active: active, box: box}
       else
         render partial: "partials/tabs_index"
       end
     else
-    render partial: "partials/logged_out_tab" if !(user_signed_in?)
+    render partial: "partials/tabs_logged_out" if !(user_signed_in?)
     end
   end
 
   def horizontaltabsorter(active:, box: )
     if user_signed_in?
       if box.present?
-        render partial: "partials/tabs_horizontal", locals: {active: active, box: box}
+        render partial: "partials/tabs_box_horizontal", locals: {active: active, box: box}
       else
         render partial: "partials/tabs_index_horizontal"
       end
     else
-      render partial: "partials/logged_out_tab_horizontal" if !(user_signed_in?)
+      render partial: "partials/tabs_logged_out_horizontal" if !(user_signed_in?)
     end
   end
 
