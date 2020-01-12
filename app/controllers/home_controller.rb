90348ac9c6c302be57535d829dcd7b8aca6b38d7
William Hancharek <william.hancharek@gmail.com>
2020-01-12 00:56:26 -0500

    made the create new box on the front page and user's boxes page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!, :only => [:index]
 
   def index
+    @box = current_user.boxes.build
     @boxes = Box.where(public: "true").take(12)
   end
 
